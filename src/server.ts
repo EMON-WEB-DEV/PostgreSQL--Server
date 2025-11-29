079f0455dcf75620b9e1efc6decc091eae922784
EMON <jakariachowdhury2427@gmail.com>
2025-11-30 02:29:48 +0600

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,39 @@
 import express, { Request, Response } from "express"
+import {Pool} from "pg"
+import dotenv from "dotenv"
+import path from "path";
+
+dotenv.config({path:path.join(process.cwd() , '.env')});
+
 const app = express()
 const port = 5050
+
+
+
+const pool = new Pool({
+  connectionString: `${process.env.CON_STR}`
+});
+
+const testDBConnection = async () => {
+    await pool.query(`CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        age INT NOT NULL,
+        email VARCHAR(100) UNIQUE NOT NULL,
+        address TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )`);
+    console.log("Connected to the database and ensured 'users' table exists.");
+};
+
+testDBConnection();
+
+
+
+
+
+
 
 // Middleware to parse JSON bodies
 app.use(express.json());
